4c01f1e68eaa77b0035036a9daa0d0533e566775
Kurtukov Pavel <pavel@kurtukov.ru>
2024-04-23 00:59:35 +0700

    Initial commit

---
+++ src/test/java/ru/kurtukov/hibernatedao/HibernateDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kurtukov.hibernatedao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
